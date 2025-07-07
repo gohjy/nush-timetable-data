46e2e209d09d04980b51fd878012aaafda262e8c
gohjy <154421097+gohjy@users.noreply.github.com>
2025-07-07 17:16:15 +0800

    Update to include courseCode field

--- json_convert.js
+++ json_convert.js
@@ -1,7 +1,7 @@
 // This function is intended to convert
 // JSON output in ... { ... "p1": "NAME", "p2": "NAME"... }
 // (from csvjson.com) to the format
-// { ... "p1": { "subject": "NAME" }, "p2": { "subject": "NAME"}... }
+// { ... "p1": { "courseCode": "", "subject": "NAME" }, "p2": { "courseCode": "", "subject": "NAME"}... }
 // which is used in the data files.
 // Copy and paste this code into the browser console
 // then run convert(data) for each data object
@@ -11,7 +11,7 @@ function convert(data) {
   for (let i=0; i<data.data.length; i++) {
     for (let k of Object.keys(data.data[i])) {
       if (k.match(/^p\d+/)) { // "p1", "p2" etc
-         data.data[i][k] = { "subject": data.data[i][k] }
+        data.data[i][k] = { "courseCode": "", "subject": data.data[i][k] };
       }
     }
   }
